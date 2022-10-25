e0edfe09442be86ec8ec94669ea27eb11b41429d
Pasi Lammi <pasi@pashi.net>
2022-10-25 17:09:11 +0300

    upd

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "vmset_name_prefix" {
+    value = var.name_prefix
+}
+output "vmset_dns_domain" {
+    value = var.dns_domain
+}
+output "vmcount" {
+    value = var.vmcount
+}
+output "vm_id" {
+    value = azurerm_linux_virtual_machine.vm.*.id
+}
---
+++ var.tf
@@ -0,0 +1,9 @@
+variable "name_prefix" {}
+variable "dns_domain" {}
+variable "location" {}
+variable "resource_group_name" {}
+variable "vmcount" {}
+variable "subnet_id" {}
+variable "virtual_machine_size" {}
+variable "virtual_machine_admin_username" {}
+variable "virtual_machine_admin_public_key" {}
